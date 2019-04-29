127ca3bd057f007cc8c7b18fb93e24410e26ef29
des5141 <rhea31@naver.com>
2019-04-30 08:15:09 +0900

    로그인 부분 수정중

--- Server/Server/Source/SignalEvent/Func/Login.cs
+++ Server/Server/Source/SignalEvent/Func/Login.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CGD;
 using Server.Source.Core;
+using Server.Source.Database.Event;
 
 namespace Server.Source.SignalEvent.Func
 {
@@ -10,7 +11,25 @@ namespace Server.Source.SignalEvent.Func
         {
             Msg[Signal.Login] = (user, requestinfo, buffer) =>
             {
+                var playerId = buffer.extract_gmlstring();
+                var playerNickname = buffer.extract_gmlstring();
 
+                if (DatabaseCheckCanLogin.Func(playerId).Result)
+                {
+                    // 로그인 가능
+                }
+                else
+                {
+                    // 로그인 실패
+                    if (DatabaseRegister.Func(playerId, playerNickname).Result)
+                    {
+                        // 성공적으로 만듬
+                    }
+                    else
+                    {
+                        // 못 만듬. 정지당했거나 로그인 중 인지 확인 필요
+                    }
+                }
             };
         }
     }
