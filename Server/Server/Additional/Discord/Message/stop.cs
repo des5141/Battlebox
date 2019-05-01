ac207ee85a8e9ad388746f9251b39a11f22a6a62
des5141 <rhea31@naver.com>
2019-05-01 23:49:13 +0900

    Update stop.cs

--- Server/Server/Additional/Discord/Message/stop.cs
+++ Server/Server/Additional/Discord/Message/stop.cs
@@ -20,8 +20,7 @@ namespace Server.Additional.Discord.Message
                         .WithDescription("서버의 **종료 명령**이 시작되었습니다")
                         .WithTimestamp(DateTimeOffset.Now)
                         .Build();
-                    Program.Discord.GetGuild(573111073616560128).GetTextChannel(573111191468245002).SendMessageAsync("", embed: embed);
-
+                    Program.Discord.GetGuild(573111073616560128).GetTextChannel(573111191468245002).SendMessageAsync("", embed: embed).Wait();
 
                     // 넌.. 죽었다..
                     System.Diagnostics.Process.GetCurrentProcess().Kill();
