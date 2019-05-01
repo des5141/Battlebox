f6e4fb60d459a72b625609a1d85de02a53e258bd
des5141 <rhea31@naver.com>
2019-05-01 23:50:56 +0900

    Update stop.cs

--- Server/Server/Additional/Discord/Message/stop.cs
+++ Server/Server/Additional/Discord/Message/stop.cs
@@ -22,6 +22,7 @@ namespace Server.Additional.Discord.Message
                         .Build();
                     Program.Discord.GetGuild(573111073616560128).GetTextChannel(573111191468245002).SendMessageAsync("", embed: embed).Wait();
 
+
                     // 넌.. 죽었다..
                     System.Diagnostics.Process.GetCurrentProcess().Kill();
                 }
