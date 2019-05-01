caf5eb683812c8e8dfe0778000b81c56753b2a40
des5141 <rhea31@naver.com>
2019-05-01 20:03:42 +0900

    database replace

--- Server/Server/Source/Event/ServerStarted.cs
+++ Server/Server/Source/Event/ServerStarted.cs
@@ -12,7 +12,7 @@ namespace Server.Source.Event
     {
         public static void Func()
         {
-            Main.Database = new SqlManager("Server=61.84.196.75;Port=20001;Database=battlebox;Uid=rhea31;Pwd=Rheapass5141*;");
+            Main.Database = new SqlManager("Server=127.0.0.1;Port=20001;Database=battlebox;Uid=rhea31;Pwd=vkcjs8688;");
         }
     }
 }
