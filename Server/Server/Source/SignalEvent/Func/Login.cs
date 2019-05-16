0bf62e9261aeb572fe56b10a2100f3647f68f667
des5141 <rhea31@naver.com>
2019-05-16 18:01:49 +0900

    Update Login.cs

--- Server/Server/Source/SignalEvent/Func/Login.cs
+++ Server/Server/Source/SignalEvent/Func/Login.cs
@@ -14,6 +14,7 @@ namespace Server.Source.SignalEvent.Func
             {
                 Discord.SendLog("test");
 
+
                 var playerId = buffer.extract_gmlstring();
                 var playerNickname = buffer.extract_gmlstring();
 
